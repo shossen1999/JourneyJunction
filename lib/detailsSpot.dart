4a1e75e1298e1b10f1eefbd263bc1c4d34437ef4
shossen1999 <shossen1999@gmail.com>
2024-07-02 13:30:02 +0600

    button

--- lib/detailsSpot.dart
+++ lib/detailsSpot.dart
@@ -33,87 +33,121 @@ class DetailsSpotPage extends StatelessWidget {
           Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
 
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    Image.network(
-                      data['ImageUrl'] ?? 'https://via.placeholder.com/600x400',
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: 250,
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      left: 10,
-                      child: Container(
-                        color: Colors.black.withOpacity(0.5),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: Text(
-                          data['Tourist_spot_name'] ?? 'No name',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 16),
-                      Row(
+                      Stack(
                         children: [
-                          Icon(Icons.location_on, color: Colors.blueAccent),
-                          SizedBox(width: 8),
-                          Expanded(
-                            child: Text(
-                              'Country: ${data['country_name'] ?? 'Unknown'}',
-                              style: TextStyle(
-                                  fontSize: 18, color: Colors.blueAccent),
+                          Image.network(
+                            data['ImageUrl'] ??
+                                'https://via.placeholder.com/600x400',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 250,
+                          ),
+                          Positioned(
+                            bottom: 10,
+                            left: 10,
+                            child: Container(
+                              color: Colors.black.withOpacity(0.5),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              child: Text(
+                                data['Tourist_spot_name'] ?? 'No name',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      SizedBox(height: 8),
-                      Row(
-                        children: [
-                          Icon(Icons.map, color: Colors.blueAccent),
-                          SizedBox(width: 8),
-                          Expanded(
-                            child: Text(
-                              'Location: ${data['location'] ?? 'Unknown'}',
-                              style: TextStyle(
-                                  fontSize: 18, color: Colors.blueAccent),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 16),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on,
+                                    color: Colors.blueAccent),
+                                SizedBox(width: 8),
+                                Expanded(
+                                  child: Text(
+                                    'Country: ${data['country_name'] ?? 'Unknown'}',
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.blueAccent),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        'Description:',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        data['description'] ?? 'No description available.',
-                        style: TextStyle(fontSize: 16),
-                        textAlign: TextAlign.justify,
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Icon(Icons.map, color: Colors.blueAccent),
+                                SizedBox(width: 8),
+                                Expanded(
+                                  child: Text(
+                                    'Location: ${data['location'] ?? 'Unknown'}',
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.blueAccent),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Description:',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              data['description'] ??
+                                  'No description available.',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent, // Background color
+                    padding: EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(12), // Rounded corners
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      '/home', // Ensure this is the route name for HomePage
+                      (route) => false, // Remove all the previous routes
+                    );
+                  },
+                  child: Text(
+                    'Go Back to Home',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
